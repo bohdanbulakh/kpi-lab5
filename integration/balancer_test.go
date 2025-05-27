874b3e5be26c64606841c255375f5f64710aa448
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-05-27 22:57:15 +0300

    feature: created server for database

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -1,7 +1,9 @@
 package integration
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"testing"
@@ -11,6 +13,7 @@ import (
 )
 
 const baseAddress = "http://balancer:8090"
+const teamKey = "dreamteam"
 
 var client = http.Client{
 	Timeout: 3 * time.Second,
@@ -21,20 +24,35 @@ func TestBalancer(t *testing.T) {
 		t.Skip("Integration test is not enabled")
 	}
 
-	requestCount := 20
+	time.Sleep(5 * time.Second)
+
+	requestCount := 10
 	seenBackends := make(map[string]bool)
+	expectedValue := ""
 
 	for i := 0; i < requestCount; i++ {
-		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data?key=%s", baseAddress, teamKey))
 		assert.NoError(t, err, "Request %d failed", i+1)
-		assert.NotNil(t, resp, "Response should not be nil for request %d", i+1)
+		assert.Equal(t, http.StatusOK, resp.StatusCode, "Unexpected status on request %d", i+1)
 
 		if resp != nil {
 			backend := resp.Header.Get("lb-from")
 			assert.NotEmpty(t, backend, "Request %d missing lb-from header", i+1)
-			t.Logf("Request %d served by [%s]", i+1, backend)
 			seenBackends[backend] = true
-			_ = resp.Body.Close()
+
+			body, _ := io.ReadAll(resp.Body)
+			resp.Body.Close()
+
+			var result map[string]string
+			_ = json.Unmarshal(body, &result)
+			val := result["value"]
+
+			assert.NotEmpty(t, val, "Expected non-empty value from DB")
+			if i == 0 {
+				expectedValue = val
+			} else {
+				assert.Equal(t, expectedValue, val, "Expected consistent value from DB")
+			}
 		}
 	}
 
